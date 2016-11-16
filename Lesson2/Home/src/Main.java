f232c735e70ed5c73722bbc62dacdc4b95fb4fd3
Oskar <Oskar@Oskar-Komputer>
2016-11-16 17:51:57 +0100

    Zadanie domowe lekcja 1

---
+++ Lesson2/Home/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String imie; // w nim zapiszemy swoje imie
+		Scanner odczyt = new Scanner(System.in); // obiekt do odbierania danych
+													// przez uzytkownika
+
+		imie = odczyt.nextLine();
+
+		System.out.println("Witaj" + imie); // wyswietlamy powitanie
+	}
+
+}
