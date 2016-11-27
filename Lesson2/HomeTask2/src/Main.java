9d691c78bf7503f077f407aae660493b10559c24
Oskar <Oskar@Oskar-Komputer>
2016-11-27 01:40:51 +0100

    Nie potrafiłem więcej zrobić dlatego zrobiłem chociaż to co umiałem ;)

--- Lesson2/HomeTask2/src/Main.java
+++ Lesson2/HomeTask2/src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		char firstLetterOfName = 'O';
+		String lastName = "Ziêba";
+		char temperature = '1';
+		String numberOfStudentsInClass = "23";
+		String bodyWeight = "39kg";
+		String height = "147cm";
+		String heightOfMontEverest = "884800cm";
+		String numberOfNeuronsInHumanBrain = "100miliardów";
+		String piNumber = "3,14";
+		String PeopleOnFacebook = "1,5miliarda";
+		
+		System.out.println(firstLetterOfName);
+		System.out.println(lastName);
+		System.out.println(temperature);
+		System.out.println(numberOfStudentsInClass);
+		System.out.println(bodyWeight);
+		System.out.println(height);
+		System.out.println(heightOfMontEverest);
+		System.out.println(numberOfNeuronsInHumanBrain);
+		System.out.println(piNumber);
+		System.out.println(PeopleOnFacebook);
 	}
 
 }
