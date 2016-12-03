52574d15af057df740b4ad4d46c460c0539876d7
Oskar <Oskar@Oskar-Komputer>
2016-12-03 18:34:54 +0100

    Lekcja3 -> Zadanie 2 do Domu

---
+++ Lesson3/HomeTask2/src/Main.java
@@ -0,0 +1,31 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DZIENNICZEK UCZNIA");
+		System.out.println();
+		
+		String name = "Bartek";
+		String lastName = "Kowalski";
+		String school = "sp28";
+		String scienceLevel = "podstawówka";
+		char classNr = '6';
+		String classProfile = "ogolna";
+		
+		System.out.println("imie " + name);
+		System.out.println("nazwisko " + lastName);
+		System.out.println("szko³a " + school);
+		System.out.println("poziom nauki " + scienceLevel);
+		System.out.println("numer klasy " + classNr);
+		System.out.println("profil klasy " + classProfile);
+		System.out.println("wszystkie lekcje: polski, matematyka, przyroda, WF,"
+		+ "historia, muzyka, plastyka, j.angielski, religia, technika, zaj.komp");
+		System.out.println("zajecia dodatkowe: ");
+		
+		
+		
+	}
+
+}
